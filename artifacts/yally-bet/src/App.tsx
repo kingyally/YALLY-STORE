b724bbbcc39cb644c0a4a862afad2262f890225c
YALLY BET Auto <seif83470@gmail.com>
2026-04-21 20:47:30 +0000

    auto: mabadiliko 2026-04-21 20:47:30

--- artifacts/yally-bet/src/App.tsx
+++ artifacts/yally-bet/src/App.tsx
@@ -5,22 +5,25 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster as SonnerToaster } from "sonner";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { LanguageProvider } from "@/lib/i18n";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-        <Toaster />
-        <SonnerToaster position="top-center" theme="dark" />
-      </TooltipProvider>
+      <LanguageProvider>
+        <TooltipProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+          <Toaster />
+          <SonnerToaster position="top-center" theme="dark" />
+        </TooltipProvider>
+      </LanguageProvider>
     </QueryClientProvider>
   );
 }
